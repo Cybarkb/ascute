878b17aa893442f87e32ffab78712e56fca8be47
Bishnu Bachhar <bishnu.procogs@gmail.com>
2021-07-17 20:54:24 +0530

    Inner Component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,27 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
+import { CmsComponent } from './cms/cms.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { ReviewsComponent } from './reviews/reviews.component';
+import { PrivacyComponent } from './privacy/privacy.component';
+import { TermsComponent } from './terms/terms.component';
+import { FaqsComponent } from './faqs/faqs.component';
+import { ProductsComponent } from './products/products.component';
 
 const routes: Routes = [
   {path : "", redirectTo : 'home',pathMatch: 'full'},
-  {path : 'home', component : HomeComponent}, 
+  {path : 'home', component : HomeComponent},
+  {path : 'cms', component : CmsComponent},
+  {path : 'about-us', component : AboutUsComponent},
+  {path : 'contact-us', component : ContactUsComponent},
+  {path : 'reviews', component : ReviewsComponent},
+  {path : 'privacy', component : PrivacyComponent},
+  {path : 'terms', component : TermsComponent},
+  {path : 'faqs', component : FaqsComponent},
+  {path : 'products', component : ProductsComponent},
+  {path : '**', redirectTo : 'home'},
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,22 +2,39 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { BreadcrumbComponent } from './breadcrumb/breadcrumb.component';
 import { HomeComponent } from './home/home.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CmsComponent } from './cms/cms.component';
 import { AboutUsComponent } from './about-us/about-us.component';
 import { ContactUsComponent } from './contact-us/contact-us.component';
+import { ReviewsComponent } from './reviews/reviews.component';
+import { PrivacyComponent } from './privacy/privacy.component';
+import { TermsComponent } from './terms/terms.component';
+import { FaqsComponent } from './faqs/faqs.component';
+import { ProductsComponent } from './products/products.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
+    BreadcrumbComponent,
     HomeComponent,
+    CmsComponent,
     AboutUsComponent,
-    ContactUsComponent
+    ContactUsComponent,
+    ReviewsComponent,
+    PrivacyComponent,
+    TermsComponent,
+    FaqsComponent,
+    ProductsComponent,
+    
+    
   ],
   imports: [
     BrowserModule,
